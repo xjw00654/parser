e8b519fdbcf109a91b9ec4e901f12ba2be8ebe4c
jwxie <xiejiawei000@gmail.com>
2022-01-03 16:37:10 +0800

    [update] webdriver version crawler;

---
+++ cdn_crawler_sel.py
@@ -0,0 +1,97 @@
+# coding: utf-8
+# author: jwxie - xiejiawei000@gmail.com
+import logging
+import os
+import random
+import re
+import time
+
+import selenium.webdriver.common.by
+from selenium import webdriver
+
+driver = webdriver.Chrome()
+
+
+def get_logger(with_file_log=True):
+    logger = logging.getLogger('CDN_CRAWLER')
+    logger.setLevel(logging.INFO)
+    formatter = logging.Formatter('%(asctime)s - %(pathname)s[line:%(lineno)d] - %(levelname)s: %(message)s')
+
+    if with_file_log:
+        os.makedirs('logs', exist_ok=True)
+        ct = time.localtime()
+        fh = logging.FileHandler(f'logs/{ct.tm_mon}-{ct.tm_mday}__{ct.tm_hour}_{ct.tm_min}_{ct.tm_sec}.log',
+                                 encoding='utf-8')
+        fh.setLevel(logging.DEBUG)
+        fh.setFormatter(formatter)
+
+        logger.addHandler(fh)
+
+    ch = logging.StreamHandler()
+    ch.setLevel(logging.DEBUG)
+    ch.setFormatter(formatter)
+    logger.addHandler(ch)
+
+    return logger
+
+
+def sleep_module():
+    p = logging.getLogger('CDN_CRAWLER')
+
+    delay = random.randint(1, 3) + random.random()
+    p.info(f'休息{delay}秒.')
+    time.sleep(delay)
+    if random.random() < 0.2:
+        delay2 = random.random() * 3
+        p.info(f'命中红心！喝口水，多休息休息一下~，大约{delay2}秒')
+        time.sleep(delay2)
+
+    p.info('休息结束，开始工作！')
+
+
+LOGGER = get_logger()
+DEFAULT_UA = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 ' \
+             '(KHTML, like Gecko) Chrome/96.0.4664.110 Safari/537.36 Edg/96.0.1054.62'
+BASE_URL = 'https://cdn.chinaz.com'
+DEFAULT_PROXY_PROFILE = {
+    'http': 'http://127.0.0.1:7890',
+    'https': 'http://127.0.0.1:7890',
+}
+
+driver.get(url=BASE_URL)
+
+LOGGER.info('请手动登录!!!')
+time.sleep(20)
+
+toplist = driver.find_element('class name', 'toplist-main')
+
+cdn_list = toplist.find_elements('class name', 'ullist')
+_T = '\n'.join([e.text.split('\n')[0] for e in cdn_list])
+LOGGER.info(f"在首页，找到{len(cdn_list)}个cdn服务商，分别为:\n {_T}")
+
+cdn_list_href = [e.find_element_by_tag_name('a').get_attribute('href') for e in cdn_list]
+cdn_list_name = [e.find_element_by_tag_name('a').text for e in cdn_list]
+
+provider_cdn_data = {k: [] for k in cdn_list_name}
+for name, href in zip(cdn_list_name, cdn_list_href):
+    driver.get(href)
+
+    total_ip_counts = int([e for e in re.findall(r'\d*', driver.find_element('id', 'areaipcount').text) if e][0])
+    total_pages = int(total_ip_counts // 20)
+    LOGGER.info(f'{name}一共有{total_pages}页数据，共计{total_ip_counts}条CDN ip数据')
+    for page in range(2, total_pages + 1):
+        LOGGER.info(f'正在处理{name}的第{page}/{total_pages}页...')
+        box_data = driver.find_element('class name', 'box')
+        ips = [e.text for e in box_data.find_elements('tag name', 'li')]
+        provider_cdn_data[name].extend(ips)
+
+        next_page_bottom = [e for e in driver.find_element(webdriver.common.by.By.ID, 'pagelist').find_elements(
+            webdriver.common.by.By.TAG_NAME, 'a') if e.text == '>'][0]
+        next_page_bottom.click()
+        sleep_module()
+        if page % 100 == 0:
+            os.makedirs(f'results', exist_ok=True)
+            with open(f'results/cdn_{name}.txt', 'w', encoding='utf-8') as fp:
+                for line in provider_cdn_data[name]:
+                    fp.write(line.replace('\n', '\t') + '\n')
+    LOGGER.info(f'{name}供应商爬取结束，共计{len(provider_cdn_data[name])}条数据.')
